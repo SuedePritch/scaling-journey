6f2f8c6d6047f22e60ae32f27511e77186a7e30d
Your Name <james.edward.pritchard@gmail.com>
2022-06-11 17:30:56 -0400

    Employee class

---
+++ classes/Employee.js
@@ -0,0 +1,8 @@
+class Employee{
+    constructor(name, email, employeeId){
+        this.name = name,
+        this.email = email,
+        this.employeeId = employeeId
+    }
+}
+module.exports = Employee
