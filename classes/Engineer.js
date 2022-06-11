8bd40bc7d5fc5d70a95bd2f97c2c061f0c4f7100
Your Name <james.edward.pritchard@gmail.com>
2022-06-11 17:35:44 -0400

    Engineer Class

---
+++ classes/Engineer.js
@@ -0,0 +1,9 @@
+const Employee = require('./Employee')
+
+class Engineer extends Employee{
+    constructor(name, email, employeeId, github){
+        super(name,email,employeeId);
+        this.github = github
+    }
+}
+module.exports = Engineer
